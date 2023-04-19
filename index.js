a9d96aacdcb915791eebc31a200743d35d8f23c6
Cody Delzer <delzercody@gmail.com>
2023-04-18 21:36:23 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+    let p = document.querySelector('#text')
+   console.log(p)
+    p.textContent = 'This is really cool!'
+})
